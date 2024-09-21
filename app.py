fd31007d2928446f0fd1b0ec73643c9e456ce81d
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-20 20:52:35 -0500

    Añadir class Jugador y la ruta para registrar jugadores

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+jugadores_registrados = []
+
+class Jugador(BaseModel):
+    nombre: str
+
+@app.post("/jugadores/")
+def registrar_jugador(jugador: Jugador):
+    if jugador.nombre in [j.nombre for j in jugadores_registrados]:
+        raise HTTPException(status_code=400, detail="El jugador ya está registrado")
+    jugadores_registrados.append(jugador)
+    return {"mensaje": f"Jugador {jugador.nombre} registrado con éxito"}
 
 @app.get("/")
 def read_root():

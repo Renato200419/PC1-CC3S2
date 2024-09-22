f26e0941e922b528139c68d077220df30f655a8c
ISMA12345830 <ismael_llr_05@hotmail.com>
2024-09-21 23:35:06 -0500

    Se agrega la funcion mostrar estadisticas

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import uvicorn
 def jugar_consola():
     print("Bienvenido al Juego de Dados Competitivo")
 
+def mostrar_estadisticas():
+    id_partida = int(input("Ingresa el ID de la partida para ver estadísticas: "))
+    respuesta = requests.get(f"{API_BASE_URL}/partidas/{id_partida}/estadisticas")
+    print("Puntuaciones:", respuesta.json().get('puntajes'))
+
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
     jugar_consola()

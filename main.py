988a08b7f26fe4b9d85fda4b3ac2f75d2d9d8edb
Renato200419 <renato.olivera.c@uni.pe>
2024-09-21 23:34:38 -0500

    Se agrega la función crear-partida

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import uvicorn
 def jugar_consola():
     print("Bienvenido al Juego de Dados Competitivo")
 
+def crear_partida():
+    numero_de_jugadores = int(input("Ingresa el número de jugadores: "))
+    jugadores = []
+    for _ in range(numero_de_jugadores):
+        nombre = input("Ingresa el nombre del jugador: ")
+        jugadores.append({"nombre": nombre})
+    respuesta = requests.post(f"{API_BASE_URL}/partidas/", json=jugadores)
+    print(respuesta.json().get('mensaje'))
+
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
     jugar_consola()

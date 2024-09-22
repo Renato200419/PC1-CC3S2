a513ece6f45fe1da1eb59d8f7bcd76c67d0340e4
ISMA12345830 <ismael_llr_05@hotmail.com>
2024-09-22 12:08:04 -0500

    Cambiar a test con testclient

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,27 @@
-import httpx
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
 
 def test_root():
-    response = httpx.get("http://localhost:8000/")
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Bienvenido al Juego de Dados"}
 
+
 def test_crear_partida():
     jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]
-    response = httpx.post("http://localhost:8000/partidas/", json=jugadores)
+    response = client.post("/partidas/", json=jugadores)
     assert response.status_code == 200
     assert "Partida" in response.json()["mensaje"]
 
+
 def test_lanzar_dados():
-    API_BASE_URL = "http://localhost:8000"
-    response = httpx.post(f"{API_BASE_URL}/partidas/", json=[
-        {"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}
-    ])
+    jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]
+    response = client.post("/partidas/", json=jugadores)
     partida_id = response.json()['partida']['id']
-    response = httpx.post(f"{API_BASE_URL}/partidas/{partida_id}/lanzar")
+
+    response = client.post(f"/partidas/{partida_id}/lanzar")
     assert response.status_code == 200
     assert "Dados lanzados" in response.json().get('mensaje')

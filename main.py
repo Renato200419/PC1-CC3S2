5ea52efa445a290b6cfe925f6d4ecaf1903abe95
Renato200419 <renato.olivera.c@uni.pe>
2024-09-21 22:44:27 -0500

    Se agrega la función crear-partida

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def jugar_consola():
     print("Bienvenido al Juego de Dados Competitivo")
 
+def crear_partida():
+    numero_de_jugadores = int(input("Ingresa el número de jugadores: "))
+    jugadores = []
+    for _ in range(numero_de_jugadores):
+        nombre = input("Ingresa el nombre del jugador: ")
+        jugadores.append({"nombre": nombre})
+    respuesta = requests.post(f"{API_BASE_URL}/partidas/", json=jugadores)
+    print(respuesta.json().get('mensaje'))
+
 if __name__ == "__main__":
     jugar_consola()

1809641d0c354246dc05de39e33840a6a8cd4b4b
ISMA12345830 <ismael_llr_05@hotmail.com>
2024-09-21 22:46:43 -0500

    Se agrega la funcion Mostrar estadistica

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def jugar_consola():
     print("Bienvenido al Juego de Dados Competitivo")
 
+def mostrar_estadisticas():
+    id_partida = int(input("Ingresa el ID de la partida para ver estadísticas: "))
+    respuesta = requests.get(f"{API_BASE_URL}/partidas/{id_partida}/estadisticas")
+    print("Puntuaciones:", respuesta.json().get('puntajes'))
+
 if __name__ == "__main__":
     jugar_consola()

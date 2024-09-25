749a1c0784a18b3cff65397384a685684f1b0a42
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-24 20:37:24 -0500

    Revert "Añadir metricas para el conteo partidas creadas"
    This reverts commit dcb2be4ff8a918c431381d79af0cf2de237356f6.

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ class Partida(BaseModel):
     puntajes: dict
 
 # Métricas
-partidas_counter = Counter("partidas_creadas_totales", "Total de partidas creadas")
 tiradas_counter = Counter("tiradas_totales", "Total de tiradas realizadas")
 latencia_histogram = Histogram("latencia_api", "Latencia de la API en segundos")
 
@@ -41,7 +40,6 @@ def crear_partida(jugadores: List[Jugador]):
         raise HTTPException(status_code=400, detail="Se requieren al menos 2 jugadores")
     nueva_partida = Partida(id=len(partidas) + 1, jugadores=jugadores, puntajes={jugador.nombre: 0 for jugador in jugadores})
     partidas.append(nueva_partida)
-    partidas_counter.inc()
     return {"mensaje": f"Partida {nueva_partida.id} creada con éxito", "partida": nueva_partida}
 
 @app.post("/partidas/{partida_id}/lanzar")

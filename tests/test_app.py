49ba571625e35c01f8f7365311444778d62b231e
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-28 23:20:50 -0500

    fix url

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,22 @@
+import os
 from fastapi.testclient import TestClient
 from app.main import app
 
-client = TestClient(app)
+# Forzar la configuración de la URL de la base de datos a localhost
+os.environ["DATABASE_URL"] = "postgresql://admin:secret@localhost:5432/dadosdb"
 
+client = TestClient(app)
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Bienvenido al Juego de Dados"}
 
-
 def test_crear_partida():
     jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]
     response = client.post("/partidas/", json=jugadores)
     assert response.status_code == 200
     assert "Partida" in response.json()["mensaje"]
-
 
 def test_lanzar_dados():
     jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]

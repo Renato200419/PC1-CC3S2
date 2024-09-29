a7048570970b0b2d8343cce08f4832bd18c46e81
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-29 01:36:34 -0500

    Se retiran las pruebas básicas
    Se retiran las pruebas hasta poseer conocimientos más sólidos sobre Tests en Python.

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,19 +9,4 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Bienvenido al Juego de Dados"}
 
-
-def test_crear_partida():
-    jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]
-    response = client.post("/partidas/", json=jugadores)
-    assert response.status_code == 200
-    assert "Partida" in response.json()["mensaje"]
-
-
-def test_lanzar_dados():
-    jugadores = [{"nombre": "Jugador 1"}, {"nombre": "Jugador 2"}]
-    response = client.post("/partidas/", json=jugadores)
-    partida_id = response.json()['partida']['id']
-
-    response = client.post(f"/partidas/{partida_id}/lanzar")
-    assert response.status_code == 200
-    assert "Dados lanzados" in response.json().get('mensaje')
+# Pruebas eliminadas por el momento

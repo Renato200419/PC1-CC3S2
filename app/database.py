721fc158496802b1327028ea277fa9664e6244d7
Renato Steven Olivera Calderón <renato.olivera.c@uni.pe>
2024-10-07 15:19:59 -0500

    Reubicación de importaciones en database.py para evitar dependencia circular

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from .config import db  # Importar la configuración de la base de datos desde config.py
-from .models import Jugador, Partida, JugadoresPartidas
 
 # Inicialización de la base de datos y creación de tablas
 def initialize_db():
+    # Importar localmente los modelos para evitar la dependencia circular
+    from .models import Jugador, Partida, JugadoresPartidas
     db.connect()
     db.create_tables([Jugador, Partida, JugadoresPartidas])

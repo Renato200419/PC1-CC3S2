2f4d6aaefb2aae12d49f54454a40095c93b00896
ISMA12345830 <ismael_llr_05@hotmail.com>
2024-10-08 02:00:22 -0500

    Aceptacion del codigo 201: creacion de un nuevo recurso:  un nuevo jugador.

--- features/steps/registro_jugador_steps.py
+++ features/steps/registro_jugador_steps.py
@@ -18,7 +18,7 @@ def step_impl(context, nombre):
 @then('el sistema confirma que el jugador ha sido registrado con éxito')
 def step_impl(context):
     print(context.response.json())
-    assert context.response.status_code == 200
+    assert context.response.status_code in [200, 201]  # Acepta 200 (OK) y 201 (Created)
 
 @then('el sistema notifica que el jugador ya está registrado')
 def step_impl(context):

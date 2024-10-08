5b87cbc38d2059a80510f765f96cf64cfadc1793
ISMA12345830 <ismael_llr_05@hotmail.com>
2024-10-08 01:30:04 -0500

    Mejoramiento de la funcionalidad del database.py

--- app/database.py
+++ app/database.py
@@ -4,5 +4,6 @@ from .config import db  # Importar la configuración de la base de datos desde c
 
 # Inicialización de la base de datos y creación de tablas
 def initialize_db():
+    from .models import Jugador, Partida, JugadoresPartidas  # Importar dentro de la función para evitar circularidad
     db.connect()
     db.create_tables([Jugador, Partida, JugadoresPartidas])
